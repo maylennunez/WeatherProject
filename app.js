92cbcbd3c2e051d6862cbea6b0405a2886d1cf0a
Maylen Nunez <maylennunez@hotmail.com>
2020-08-24 14:37:07 -0400

    get request with the  node https module

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require("express");
-
+const https = require("https");
 const app = express();
 
 
@@ -7,7 +7,11 @@ const app = express();
 app.get("/", function(req, res){
   res.send("Server is up and running.")
 });
+const url = "https://api.openweathermap.org/data/2.5/weather?q=Miami&appid=071ae9990f00873fef062f6bf6095f41"
 
+https.get(url, function(response){
+  console.log(url);
+});
 
 
 app.listen(3000, function(){
