32bc1a37fa1f76fe29ed53588923b6773cdc4168
Maylen Nunez <maylennunez@hotmail.com>
2020-08-27 17:35:49 -0400

    post request

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
 const express = require("express");
 const https = require("https");
+const bodyParser = require("body-parser")
+
 const app = express();
 
-
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req, res) {
+res.sendFile(__dirname + "/index.html")
+});
+
+app.post("/", function(req, res){
+  console.log(req.body.cityName)
+  const apiKey =" 071ae9990f00873fef062f6bf6095f41";
+  const query = req.body.cityName
+  const units = "imperial"
+  const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + apiKey + "&units=" + units;
 
 
-  const url = "https://api.openweathermap.org/data/2.5/weather?q=Miami&appid=071ae9990f00873fef062f6bf6095f41&units=imperial"
 
   https.get(url, function(response) {
     console.log(response.statusCode); //
@@ -25,16 +35,16 @@ app.get("/", function(req, res) {
       const temp = weatherData.main.temp
       const weatherDescription = weatherData.weather[0].description
       const icon = weatherData.weather[0].icon
-      const imageUrl =  "http://openweathermap.org/img/wn/" + icon + "@2x.png"
+      const imageURL =  "http://openweathermap.org/img/wn/" + icon + "@2x.png"
       res.write("<p> The weather is currently " + weatherDescription + " <p>")
-      res.write("<h1>The temperture in Miami is " + temp + " degrees Fahrenheit</h1>")
-      res.write("<img src=" + imageUrl + ">");
+      res.write("<h1>The temperture in "+ query +"  is " + temp + " degrees Fahrenheit</h1>")
+      res.write("<img src=" + imageURL + ">");
       res.send()
     })
   })
-
-  //res.send("Server is up and running.")
 })
+  //res.send("Server is up and running.")
+
 
 
 
